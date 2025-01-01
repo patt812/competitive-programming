c1f3f859b25cfe0886653b6c758ea5f3ff3a6e55
patt812 <dev.patt812@gmail.com>
2025-01-01 16:32:55 +0900

    Add array library bubbleSort

--- libs/array.py
+++ libs/array.py
@@ -26,3 +26,29 @@ def insertionSort(a: typing.List[int], descending: bool) -> None:
             j -= 1
         a[j + 1] = v
 
+# Sort array by bubble sort
+def bubbleSort(a: typing.List[int]) -> None:
+    n = len(a)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(n-1, 0, -1):
+            if a[i] < a[i-1]:
+                a[i], a[i-1] = a[i-1], a[i]
+                flag = True
+
+# Sort array by bubble sort in descending order
+def bubbleSort(a: typing.List[int], descending: bool) -> None:
+    if not descending:
+        bubbleSort(a)
+        return
+
+    n = len(a)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(n-1, 0, -1):
+            if a[i] > a[i-1]:
+                a[i], a[i-1] = a[i-1], a[i]
+                flag = True
+
